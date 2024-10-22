09d052a0012ef7b45e3a5e68b8fb614c0d4fa87f
ChandPie_thecod(h)er <96528075+moonpiee@users.noreply.github.com>
2024-10-22 17:06:46 +0530

    Update app.py
    removed unused module- PromptTemplate

--- app.py
+++ app.py
@@ -12,7 +12,6 @@ from langchain_core.prompts import (
 from langchain_core.messages import SystemMessage
 from langchain.chains.conversation.memory import ConversationBufferWindowMemory
 from langchain_groq import ChatGroq
-from langchain.prompts import PromptTemplate
 
 
 def main():
